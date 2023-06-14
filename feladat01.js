ad39f9d1fa0aa385fe8f9e246c5524afcb477bd4
tothsara <tothsara92@gmail.com>
2023-06-15 00:32:52 +0200

    solution

--- feladat01.js
+++ feladat01.js
@@ -1,12 +1,22 @@
-export function feladat01(színek) {
-  if (színek.length === 0 || színek.length === 1) {
+function feladat01(colors) {
+  let length = colors.length;
+
+  if (length === 0 || length === 1) {
     return 0;
   }
 
-  if (színek.length === 2) {
-    return 1;
+  let count = 0;
+
+  for (let i = 2; i <= length; i++) {
+    let x = 1;
+    let y = 1;
+
+    for (let j = 1; j <= i; j++) {
+      x *= (length - j + 1);
+      y *= j;
+    }
+    count += (x/y);
   }
-
   
+  return count;
 }
-
